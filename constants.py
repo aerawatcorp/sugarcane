4aae598752f4bf04a13ed69e1921ec0c74624201
acpmasquerade <dhrubastergmail.com>
2024-03-19 23:37:41 +0545

    faker

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+import os 
+
+CONTENT_ROOT = os.path.join(os.path.dirname(__file__), "content/")
 DATA_NODES = ["post", "user", "food", "emoji", "people"]
 
 REDIS_HOST = "localhost"
@@ -13,4 +16,3 @@ NODES_TTL = 100
 SERVICE_HOST = "http://localhost"
 SERVICE_PORT = 8000
 LISTEN_PORT = 8000
-
