ec87a99dcbf5e4ec7d5fe0882e835d5b868c8eda
Dhruba Adhikari <acp.dhruba@gmail.com>
2024-03-22 20:14:07 +0545

    etag implementation in master is done

--- faker.py
+++ faker.py
@@ -2,20 +2,22 @@
 
 import json
 import os
+import sys
 import random
 from datetime import datetime, timedelta
 
-from constants import (CONTENT_ROOT, DATA_NODES, MASTER_KEY, MASTER_TTL,
-                       NODES_TTL)
-from helpers import r_expires_on, r_get, r_set
+from constants import CONTENT_ROOT, DATA_NODES, MASTER_KEY, MASTER_TTL, NODES_TTL
+from helpers import r_expires_on, r_get, r_set, r_master_etag, etag_master
 
 
 def get_versioned_node_url(node_name, version):
     return f"/cdn/get/{version}/{node_name}"
 
+
 def random_version():
     rand = f"{random.random() * 100000}"
     return "v" + rand[0:1] + "." + rand[1:2] + "." + rand[2:4]
+
 
 def random_versioned_node_url(node_name):
     semver = random_version()
@@ -29,6 +31,7 @@ def fake_master_data(node_urls):
         "updated_on": str(datetime.now()),
         "nodes": node_urls,
     }
+
 
 def fake_node_data(node_name, version=None, ttl=None):
     return {
@@ -57,12 +60,15 @@ def fake_all_nodes():
             "url": get_versioned_node_url(node_name, version),
             "version": version,
             "expires_on": str(r_expires_on(NODES_TTL)),
-            "updated_on": str(datetime.now())
+            "updated_on": str(datetime.now()),
         }
-        all_nodes_data[node_name] = fake_node_data(node_name, version=version, ttl=NODES_TTL)
+        all_nodes_data[node_name] = fake_node_data(
+            node_name, version=version, ttl=NODES_TTL
+        )
     return all_nodes, all_nodes_data
 
-def rebuild_all_fake_data(directory=True, conn=None):
+
+def rebuild_all_fake_data(directory=True, conn=None, node=None):
     all_nodes_info, all_nodes_data = fake_all_nodes()
     master_data = fake_master_data(all_nodes_info)
 
@@ -71,6 +77,8 @@ def rebuild_all_fake_data(directory=True, conn=None):
 
     # for nodes
     for node_name, node_data in all_nodes_data.items():
+        if node is not None and node_name != node:
+            continue
         version = node_data["version"]
         if directory:
             node_dir = os.path.join(CONTENT_ROOT, "nodes/", node_name)
@@ -82,7 +90,9 @@ def rebuild_all_fake_data(directory=True, conn=None):
                 print(f'Writing node {node_name}@{node_data["version"]} to {node_file}')
                 f.write(json.dumps(node_data, indent=4, sort_keys=True))
             with open(latest_node_file, "w") as f:
-                print(f'Writing node {node_name}@{node_data["version"]} to {latest_node_file}')
+                print(
+                    f'Writing node {node_name}@{node_data["version"]} to {latest_node_file}'
+                )
                 f.write(json.dumps(node_data, indent=4, sort_keys=True))
         if conn:
             r_set(conn, f"{node_name}:{node_data['version']}", node_data, ttl=NODES_TTL)
@@ -91,11 +101,18 @@ def rebuild_all_fake_data(directory=True, conn=None):
     if directory:
         master_file = os.path.join(CONTENT_ROOT, "master.json")
         with open(master_file, "w") as f:
-            print(f'Writing master to {master_file}')
+            print(f"Writing master to {master_file}")
             f.write(json.dumps(master_data, indent=4, sort_keys=True))
     if conn:
         r_set(conn, MASTER_KEY, master_data, ttl=MASTER_TTL)
+        r_master_etag(conn, etag_master(master_data["updated_on"]))
+
 
 if __name__ == "__main__":
     from redis_client import r1
-    rebuild_all_fake_data(directory=True, conn=r1)
+
+    try:
+        node = sys.argv[1]
+    except IndexError as exc:
+        node = None
+    rebuild_all_fake_data(directory=True, conn=r1, node=None)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,3 @@
-
 import json
 import random
 import sys
@@ -8,18 +7,28 @@ import humanize
 import redis
 import requests
 
-from constants import (DATA_NODES, LISTEN_PORT, MASTER_KEY, MASTER_TTL,EXPIRED_PREFIX,
-                        NODES_TTL, R_PREFIX, SERVICE_PORT)
-
 from flask import make_response
+
+from constants import (
+    DATA_NODES,
+    LISTEN_PORT,
+    MASTER_KEY,
+    MASTER_TTL,
+    EXPIRED_PREFIX,
+    NODES_TTL,
+    R_PREFIX,
+    SERVICE_PORT,
+)
 
 
 def verify_redis_connection(conn):
     # Verify if redis is running, by simply fetching a TTL value
     conn.ttl("{R_PREFIX}{MASTER_KEY}")
 
+
 def r_expires_on(ttl):
     return datetime.now() + timedelta(seconds=ttl)
+
 
 def r_get(conn, key):
     _key = f"{R_PREFIX}:{key}"
@@ -39,24 +48,39 @@ def r_get(conn, key):
 def r_set(conn, key, value, ttl=None):
     _key = f"{R_PREFIX}:{key}"
     conn.set(_key, json.dumps(value))
-    conn.expire(_key, ttl)
+    if ttl is not None:
+        conn.expire(_key, ttl)
+
+
+def r_master_etag(conn, value=None):
+    if value:
+        r_set(conn, f"{MASTER_KEY}:ETAG", value, ttl=None)
+    else:
+        etag, _ = r_get(conn, f"{MASTER_KEY}:ETAG")
+        return etag
+
 
 # remember if some key was expired earlier
 def r_log_expire(conn, key, ttl):
     _key = f"{EXPIRED_PREFIX}:{key}"
     r_set(conn, _key, "1", ttl=ttl)
 
+
 def r_was_expired(conn, key):
     _key = f"{EXPIRED_PREFIX}:{key}"
     value, _ = r_get(conn, _key)
     return value == "1"
 
-def json_response(data, is_json=False, headers={}):
+
+def json_response(data, is_json=False, headers={}, etag=None):
     response = make_response(json.dumps(data, indent=4) if is_json is False else data)
     response.headers["Content-Type"] = "application/json"
     for k, v in headers.items():
         response.headers[k] = v
+    if etag:
+        response.headers["Etag"] = etag
     return response
+
 
 def humanize_delta(date):
     return humanize.precisedelta(
@@ -64,3 +88,19 @@ def humanize_delta(date):
         minimum_unit="seconds",
     )
 
+
+def etag_master(updated_on):
+    _etag = updated_on.replace(" ", "").replace(":", "").replace("-", "")
+    return f"MASTER.{_etag}"
+
+
+def etag_node(node_name, version):
+    return f"NODE.{node_name.upper()}.{version}"
+
+
+def master_etag_verification(request, conn):
+    etag = request.headers.get("If-None-Match")
+    if etag:
+        stored_etag = r_master_etag(conn)
+        return stored_etag == etag
+    return None
