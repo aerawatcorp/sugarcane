b79ab78e622f0dd5c0de51578416276c1fadbad5
acpmasquerade <acp.dhruba@gmail.com>
2024-03-29 14:18:16 +0545

    import error in demoapplication views

--- demo_application/server.py
+++ demo_application/server.py
@@ -8,25 +8,30 @@ import redis
 import requests
 from flask import Flask, abort, make_response, request, json
 
-from sugarlib.constants import (DATA_NODES, MASTER_KEY, MASTER_TTL,
-                        NODES_TTL, R_PREFIX, DEMO_APP_CANE_SERVER_HOST)
+from sugarlib.constants import (
+    DATA_NODES,
+    MASTER_KEY,
+    MASTER_TTL,
+    NODES_TTL,
+    R_PREFIX,
+    DEMO_APP_CANE_SERVER_HOST,
+)
 from demo_application.helpers import fetch_master_schema, fetch_node_data
+from demo_application.views import index_view, browse_view
 
 from flask import Blueprint
 
-mock_app_blueprint = Blueprint('mock_app', __name__)
+mock_app_blueprint = Blueprint("mock_app", __name__)
+
 
 @mock_app_blueprint.route("/")
 def index():
-    from views import index_view
     master_data = fetch_master_schema()
     return index_view(ctx={"master_data": master_data, "now": datetime.now()})
 
 
 @mock_app_blueprint.route("/browse/<node_name>")
 def browse(node_name):
-    from views import browse_view
-
     master_data = fetch_master_schema()
     node_data = fetch_node_data(node_name)
     return browse_view(
