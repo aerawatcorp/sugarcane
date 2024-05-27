dab238b6294ca1e4f4c03093bffac2ee7f16a7a7
Ashin Shakya <ashins1997@gmail.com>
2024-05-27 16:00:26 +0545

    Move from sugardj to jaggery folder

--- jaggery/jaggery/asgi.py
+++ jaggery/jaggery/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sugardj project.
+ASGI config for jaggery project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sugardj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jaggery.settings')
 
 application = get_asgi_application()
--- jaggery/jaggery/urls.py
+++ jaggery/jaggery/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sugardj project.
+URL configuration for jaggery project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
