348faf42519571779fbe3ae6a51d87bfa29169cb
Ashin Shakya <ashins1997@gmail.com>
2024-05-28 15:18:46 +0545

    Add is_live and latest_version field in catalog, remove filehandler in store as cache and refactor store build node schema function

--- jaggery/sachet/exceptions.py
+++ jaggery/sachet/exceptions.py
@@ -9,3 +9,18 @@ class WriteToCacheError(CaneException):
 class CacheExpired(CaneException):
     def __init__(self, message, error_key="cache_expired"):
         super().__init__(message, error_key)
+
+
+class DatabaseCacheExpired(CaneException):
+    def __init__(self, message, error_key="cache_expired"):
+        super().__init__(message, error_key)
+
+
+class FileCacheExpired(CaneException):
+    def __init__(self, message, error_key="cache_expired"):
+        super().__init__(message, error_key)
+
+
+class RedisCacheExpired(CaneException):
+    def __init__(self, message, error_key="cache_expired"):
+        super().__init__(message, error_key)
--- jaggery/sachet/models.py
+++ jaggery/sachet/models.py
@@ -1,20 +1,19 @@
 import requests
-import os
-import traceback
 import logging
 
 from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
 from django.db import models
-from django.utils.dateparse import parse_datetime
+from django.db.models import OuterRef, Subquery, Value
+from django.db.models.functions import Coalesce
 
 from sugarlib.redis_client import r1_cane as r1
 from sugarlib.redis_helpers import r_set, r_get
 from sugarlib.constants import CONTENT_ROOT, MASTER_TTL, NODES_TTL, NODE_API_URL, MASTER_SCHEMA_PATH, MASTER_KEY
 
-from sachet.exceptions import WriteToCacheError, CacheExpired
+from sachet.exceptions import WriteToCacheError, DatabaseCacheExpired, FileCacheExpired, RedisCacheExpired
 from helpers.models import BaseModel
-from helpers.misc import get_local_time, FileHandler, build_url
+from helpers.misc import get_local_time
 
 
 logger = logging.getLogger(__name__)
@@ -22,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 class Catalog(BaseModel):
 	"""Store information related to catalog"""
-	name = models.CharField(max_length=255, db_index=True)
+	name = models.CharField(max_length=255, db_index=True, unique=True)
 	namespace = models.CharField(max_length=255, null=True, blank=True)
 	provider = models.CharField(max_length=255, db_index=True)
 	provider_url = models.URLField()
@@ -31,6 +30,8 @@ class Catalog(BaseModel):
 	trigger_rule = models.JSONField(default=dict, blank=True)
 
 	ttl = models.IntegerField(default=100, help_text=_("Default time to live in seconds for the catalog stores"))
+	is_live = models.BooleanField(default=False)
+	latest_version = models.CharField(max_length=255)
 
 	def __str__(self):
 		return self.name
@@ -48,19 +49,13 @@ class Store(BaseModel):
 	remarks = models.CharField(max_length=255, null=True, blank=True)
 
 	class Meta:
-		unique_together = ["catalog", "url", "version"]
+		unique_together = ["catalog", "version"]
 		
-	def get_cache_file_path(self):
-		return os.path.join(CONTENT_ROOT, f"nodes/{self.catalog.name}/{self.version}.json")
-	
 	def get_cache_redis_key(self):
 		return f"{self.catalog.name}-{self.version}"
 
-	def get_node_url(self):
-		return NODE_API_URL.format(node_name=self.catalog.name, version=self.version)
-
-	def get_data(self) -> dict:
-		"""Get data from request url"""
+	def request_data(self) -> dict:
+		"""Get new data from request url"""
 		response = requests.get(self.url)
 		if not response.ok:
 			logger.error(f"[STORE] Fetch data error idx - {self.idx}")
@@ -73,84 +68,71 @@ class Store(BaseModel):
 		
 		if self.expires_on < now:
 			logger.error(f"[STORE] DB cache invalid - {self.get_cache_file_path()} idx - {self.idx}")
-			raise CacheExpired("Invalid 3rd level cache")
-		
-		try:
-			data = FileHandler.read(self.get_cache_file_path())
-			if parse_datetime(data["expires_on"]) < now:
-				logger.error(f"[STORE] File cache invalid - {self.get_cache_file_path()} idx - {self.idx}")
-				raise CacheExpired("Invalid 2nd level Cache")	
-		except Exception as exp:
-			logger.error(f"[STORE] File cache validation error - {self.get_cache_file_path()} idx - {self.idx} {exp} {traceback.format_exc()}")
-			raise CacheExpired("Invalid 2nd level Cache")
+			raise DatabaseCacheExpired("Invalid database level cache")
 
 		try:
 			_, ttl = r_get(r1, self.get_cache_redis_key())
 			if ttl is False:
 				logger.error(f"[STORE] Redis cache invalid - {self.get_cache_redis_key()} idx - {self.idx}")
-				raise CacheExpired("Invalid 1st level cache")
+				raise RedisCacheExpired("Invalid redis cache")
 		except Exception as exp:
 			logger.error(f"[STORE] Redis cache validation error - {self.get_cache_redis_key()} idx - {self.idx}")
-			raise CacheExpired("Invalid 1st level cache")
+			raise RedisCacheExpired("Invalid redis cache")
 
-	def write_to_cache(self, data):
-		"""Write to cache"""
+	def build_node_schema(self):
+		"""Build node schema"""
 		now = get_local_time()
+		expires_on = now + timedelta(seconds=NODES_TTL)
+
+		return {
+			"scheme": "node",
+			"node": self.catalog.name,
+			"version": self.version,
+			"expires_on": str(expires_on),
+			"updated_on": str(self.updated_on),
+			"data": self.content
+		}
+	
+	def write_to_db(self):
+		"""Write to db"""
+		now = get_local_time()
+
+		# Get data from request
+		data = self.request_data()
 
 		# Store data in 3rd level cache
 		self.content = data
 		self.expires_on = now + timedelta(seconds=self.catalog.ttl)
 		self.save()
 
-		cache_data = {
-			"scheme": "node",
-			"node": self.catalog.name,
-			"version": self.version,
-			"expires_on": str(now),
-			"updated_on": str(self.updated_on),
-			"data": self.content
-		}
-		
-		# TODO - Need to work in cache timing
-		# Store data in 2nd level cache
-		cache_data["expires_on"] = str(now + timedelta(seconds=int(self.catalog.ttl * 0.75)))
-		FileHandler.write(self.get_cache_file_path(), cache_data)
-		
-		# Store data in 1st level cache
-		cache_data["expires_on"] = str(now + timedelta(seconds=NODES_TTL))
+	def write_to_redis(self):
+		"""Write to redis as 3rd level cache"""
+		cache_data = self.build_node_schema()
 		r_set(r1, self.get_cache_redis_key(), cache_data, ttl=self.catalog.ttl)
 	
-	@classmethod
-	def write_master_schema_to_cache(cls):
-		"""Write master schema to cache"""
-		now = get_local_time()
+	def invalidate_cache(self):
+		"""Validate cache and write to cache"""
+		try:
+			self.validate_cache()
+		except DatabaseCacheExpired:
+			logger.info(f"[STORE] Initiate db write protocol for idx - {self.idx}")
+			self.write_to_db()
+			self.write_to_redis()
 
-		cache_data = {
-			"expires_on": str(now + timedelta(seconds=MASTER_TTL)),
-			"updated_on": str(now),
-			"scheme": "master",
-			"nodes": {}
-		}
-		
-		stores = Store.objects.filter(is_obsolete=False, is_active=True).select_related("catalog")
+		except RedisCacheExpired:
+			logger.info(f"[STORE] Initiate redis cache write protocol for idx - {self.idx}")
+
+	def get_data(self):
+		"""Validate mulitple level cached data's expiry date"""
 		now = get_local_time()
 		
-		nodes = {}
-		for i in stores:
-			node_data = {
-				"expires_on": str(i.expires_on),
-				"updated_on": str(i.updated_on),
-				"url": i.get_node_url(),
-				"version": i.version
-			}
-			nodes.update({i.catalog.name: node_data})
-		
-		cache_data["nodes"] = nodes
+		data, ttl = r_get(r1, self.get_cache_redis_key())
+		if ttl is not False:
+			logger.info(f"[STORE] Get redis cache data for idx - {self.idx}")
+			return data
 
-		# TODO - Need to work in cache timing
-		# Store data in 2nd level cache
-		FileHandler.write(MASTER_SCHEMA_PATH, cache_data)
-		
-		# Store data in 1st level cache
-		cache_data["expires_on"] = str(now + timedelta(seconds=MASTER_TTL))
-		r_set(r1, {MASTER_KEY}, cache_data, ttl=MASTER_TTL)
+		if self.expires_on > now:
+			logger.info(f"[STORE] Get db cache data for idx - {self.idx}")
+			self.write_to_redis()
+			raise DatabaseCacheExpired("Invalid 3rd level cache")
+	
