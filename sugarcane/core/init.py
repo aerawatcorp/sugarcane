dd94525f198356b6b4895d57d85cbd4c3827c2bf
Ashin Shakya <ashins1997@gmail.com>
2024-05-30 14:12:17 +0545

    Remove unused flaskswaggerui comment

--- sugarcane/core/init.py
+++ sugarcane/core/init.py
@@ -14,19 +14,6 @@ def routes_initialize():
     for bp in blueprints:
         flask_app.register_blueprint(bp[0], url_prefix=bp[1])
 
-    # if config("DEBUG", cast=bool) or True:
-    #     from flask_swagger_ui import get_swaggerui_blueprint
-
-    #     SWAGGER_URL="/swagger"
-    #     swagger_ui_blueprint = get_swaggerui_blueprint(
-    #         SWAGGER_URL,
-    #         "/static/swagger.json",
-    #         config={
-    #             'app_name': 'Sugarcane API'
-    #         }
-    #     )
-    #     flask_app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
     return flask_app
 
 
