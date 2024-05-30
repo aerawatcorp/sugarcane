9fb01577a84af3d1199d4a0159296f15a6120a4b
Ashin Shakya <ashins1997@gmail.com>
2024-05-30 14:43:02 +0545

    Add logging in sugarcane

--- sugarcane/core/init.py
+++ sugarcane/core/init.py
@@ -1,7 +1,36 @@
+import os
+
+from logging.config import dictConfig
 from flask import Flask
 from decouple import AutoConfig
 
 config = AutoConfig(search_path="/app")
+
+LOG_FILE_PATH = os.path.expanduser("~") + "/sugarcane.log"
+
+dictConfig({
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+    },
+    "handlers": {
+        "default": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE_PATH,
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "django.request": {
+            "handlers": ["default"],
+            "level": "WARN",
+            "propagate": False,
+        },
+    },
+})
 
 flask_app = Flask(__name__)
 flask_app.config.from_object("core.settings")
