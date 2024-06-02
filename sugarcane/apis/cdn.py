388e159f48fdd14dc25999e282f4d5a253f62feb
Ashin Shakya <ashins1997@gmail.com>
2024-06-02 13:48:05 +0545

    Refactor master node fetch api

--- sugarcane/apis/cdn.py
+++ sugarcane/apis/cdn.py
@@ -1,28 +1,16 @@
-import requests
-
 from flask import Blueprint, abort
 from flask import request
 from urllib.parse import urlencode
 
 from core.init import flask_app
-from sugarlib.constants import (
-    MASTER_KEY,
-    MASTER_KEY_VERBOSED,
-    MASTER_TTL,
-    JAGGERY_BASE_URL,
-    MASTER_JAGGERY_API_URL,
+from sugarcane.core.helpers import json_response, get_request_data
+from sugarcane.helpers.cdn import (
+    fetch_node_data,
+    fetch_cached_node_data,
+    fetch_cached_master_data,
+    fetch_master_data,
 )
-from sugarcane.core.helpers import (
-    json_response,
-    get_request_data,
-    cache_hit_headers,
-    cache_miss_headers,
-)
-from sugarcane.helpers.cdn import fetch_node_data, fetch_cached_node_data
 from sugarcane.helpers.exceptions import ServiceUnavailableException
-from sugarlib.helpers import etag_master, build_url
-from sugarlib.redis_client import r1_cane as r1
-from sugarlib.redis_helpers import r_get, r_master_etag, r_set
 
 
 blueprint = Blueprint("cdn", __name__)
@@ -31,55 +19,15 @@ blueprint = Blueprint("cdn", __name__)
 @blueprint.route("/master/", methods=["GET"])
 def master():
     """Get nodes meta data"""
-    # Retrieve data from in memory cache
-    cached_master, ttl = r_get(r1, MASTER_KEY_VERBOSED)
-
-    if ttl is not False:
-        flask_app.logger.info("[MASTER] Return master data from cache")
-        # Return cache HIT data
-        _headers = cache_hit_headers()
-        _headers.update({"Etag": etag_master(cached_master["updated_on"])})
-        return json_response(
-            cached_master,
-            is_json=True,
-            headers=_headers,
-        )
-
-    if not MASTER_JAGGERY_API_URL:
-        abort(503, "Master data not available. Please check the configuration")
-
-    flask_app.logger.info("[MASTER] Initiate retrieve master data")
-    url = build_url(JAGGERY_BASE_URL, MASTER_JAGGERY_API_URL)
-    response = requests.get(url, headers=dict(request.headers))
-    if not response or not response.ok:
-        flask_app.logger.error(
-            f"[MASTER] JAGGERY ERROR : Could not fetch master data {response.content}"
-        )
-        abort(503, "Unable to fetch master data")
+    master_node_data = fetch_cached_master_data()
+    if master_node_data and master_node_data.status is True:
+        return master_node_data.json_response()
 
     try:
-        master_data = response.json()
-    except Exception as e:
-        flask_app.logger.error(
-            f"[MASTER] JAGGERY ERROR :  Could not parse master data {e}"
-        )
-        abort(503, "Unable to read master. Please try again later. ")
-
-    # Set in memory cache in case of cache MISS
-    flask_app.logger.info("[MASTER] Set master data in cache")
-    r_set(r1, MASTER_KEY, master_data, ttl=MASTER_TTL)
-
-    for _, v in (master_data.get("nodes") or {}).items():
-        # @TODO: Why are we popping these keys?
-        v.pop("version", None)
-        v.pop("updated_on", None)
-
-    flask_app.logger.info("[MASTER] Set master verbose data in cache")
-    r_set(r1, MASTER_KEY_VERBOSED, master_data, ttl=MASTER_TTL)
-    # TODO: The implementation of etag needs to be revisited
-    etag = etag_master(master_data["updated_on"])
-    r_master_etag(r1, etag)
-    return json_response(data=master_data, headers=cache_miss_headers(), etag=etag)
+        master_node_response = fetch_master_data()
+        return master_node_response.json_response()
+    except ServiceUnavailableException as exp:
+        abort(503, exp.message)
 
 
 @blueprint.route("/r/<version>/<node_name>", methods=["GET"])
--- sugarcane/helpers/cdn.py
+++ sugarcane/helpers/cdn.py
@@ -10,8 +10,74 @@ from sugarcane.helpers.dataclass import NodeResponse, EmptyNodeResponse
 from sugarcane.helpers.exceptions import ServiceUnavailableException
 from sugarlib.redis_client import r1_cane as r1
 from sugarlib.redis_helpers import r_set, r_get
-from sugarlib.constants import JAGGERY_BASE_URL, NODE_JAGGERY_API_URL, MASTER_TTL
-from sugarlib.helpers import etag_node, build_url
+from sugarlib.constants import (
+    JAGGERY_BASE_URL,
+    NODE_JAGGERY_API_URL,
+    MASTER_TTL,
+    MASTER_KEY_VERBOSED,
+    MASTER_JAGGERY_API_URL,
+    MASTER_KEY,
+)
+from sugarlib.helpers import etag_node, build_url, etag_master, r_master_etag
+
+
+def fetch_master_data(headers: dict = {}):
+    if not MASTER_JAGGERY_API_URL:
+        raise ServiceUnavailableException(
+            "Master data not available. Please check the configuration"
+        )
+
+    flask_app.logger.info("[MASTER] Initiate retrieve master data")
+
+    url = build_url(JAGGERY_BASE_URL, MASTER_JAGGERY_API_URL)
+    response = requests.get(url, headers=headers)
+    if not response or not response.ok:
+        flask_app.logger.error(
+            f"[MASTER] JAGGERY ERROR : Could not fetch master data {response.content}"
+        )
+        raise ServiceUnavailableException("Unable to fetch master data")
+
+    try:
+        master_data = response.json()
+    except Exception as e:
+        flask_app.logger.error(
+            f"[MASTER] JAGGERY ERROR :  Could not parse master data {e}"
+        )
+        raise ServiceUnavailableException(
+            "Unable to read master. Please try again later."
+        )
+
+    # Set in memory cache in case of cache MISS
+    flask_app.logger.info("[MASTER] Set master data in cache")
+    r_set(r1, MASTER_KEY, master_data, ttl=MASTER_TTL)
+
+    for _, v in (master_data.get("nodes") or {}).items():
+        # @TODO: Why are we popping these keys?
+        v.pop("version", None)
+        v.pop("updated_on", None)
+
+    flask_app.logger.info("[MASTER] Set master verbose data in cache")
+    r_set(r1, MASTER_KEY_VERBOSED, master_data, ttl=MASTER_TTL)
+
+    # TODO: The implementation of etag needs to be revisited
+    etag = etag_master(master_data["updated_on"])
+    r_master_etag(r1, etag)
+
+    return NodeResponse(master_data, headers=cache_miss_headers(), etag=etag)
+
+
+def fetch_cached_master_data():
+    # Retrieve data from in memory cache
+    cached_master, ttl = r_get(r1, MASTER_KEY_VERBOSED)
+
+    if ttl is not False:
+        flask_app.logger.info("[MASTER] Return master data from cache")
+        # Return cache HIT data
+        _headers = cache_hit_headers()
+        _headers.update({"Etag": etag_master(cached_master["updated_on"])})
+        return NodeResponse(data=cached_master, headers=_headers, status=True)
+
+    return EmptyNodeResponse()
 
 
 def fetch_node_data(version, node_name, sub_catalog=None, params={}, headers={}):
